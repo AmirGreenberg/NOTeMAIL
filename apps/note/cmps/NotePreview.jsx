a6893634fd610851a873fee86ebf538eaeff3ddd
DavidTalRivlin <David.dave.tal@gmail.com>
2023-12-14 14:14:43 +0200

    Delivery3 - Thursday 14:00

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -12,7 +12,7 @@ export function NotePreview({ note, onUpdateNote, onInputChange, onDoneToggle, o
 
     return (
         <article className="note-preview">
-           
+           <button></button>
            <DynamicCmp cmpType={note.type} info={note.info} noteId={note.id} onContentChange={onContentChange} onInputChange={onInputChange} onDoneToggle={onDoneToggle} onRemoveTodo={onRemoveTodo}/>
             {/* <h2 id="title" contentEditable="true" style={{ outline: "none" }} onBlur={onContentChange}>{note.info.title}</h2>
             <h4 id="txt" contentEditable="true" style={{ outline: "none" }} onBlur={onContentChange}>{note.info.txt}</h4> */}
