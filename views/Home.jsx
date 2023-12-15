af6c09b8e5f84eb911b8bd26ae66f97d145706e2
DavidTalRivlin <David.dave.tal@gmail.com>
2023-12-15 20:19:20 +0200

    css Bug fix

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,28 +1,28 @@
 
 export function Home() {
-    
-    return <section className="home">
+
+  return (
+    <section className="home">
+
+      <div className="hero">
+
+        <div className="logo-box">
+          <img src="../images/logo-white.png" alt="logo" className="logo" />
+        </div>
         
-        <div className="hero">
-      <div className="logo-box">
-        <img src="../images/logo-white.png" alt="logo" className="logo" />
-      </div>
-      <div className="text-box">
-        <h1 className="heading-primary">
-          <span className="heading-primary-main">e-mail and notes</span>
-          <span className="heading-primary-sub">Make your life easier</span>
-        </h1>
-        <div></div>
+        <div className="text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary-main">e-mail and notes</span>
+            <span className="heading-primary-sub">Note your Mail!</span>
+          </h1>
 
-        <a href="#" className="btn-hero btn-white btn-animated" onClick="onLoadGallery()"><img src="images/note.png"
+          <a href="#" className="btn-hero btn-white btn-animated" onClick={() => onLoadGallery()}><img src="images/note.png"
             className="btn-homepage" /></a>
-        <a href="#" className="btn-hero btn-white btn-animated" onClick="onLoadGallery()"><img src="images/email.png"
+          <a href="#" className="btn-hero btn-white btn-animated" onClick={() => onLoadGallery()}><img src="images/email.png"
             className="btn-homepage" /></a>
-      </div>
-    </div>
+        </div>
         
-
-
-
+      </div>
     </section>
+  )
 }
