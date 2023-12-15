8a7d1159a2b8f8a279c404d0a97802e078314a37
Amir Greenberg <amirgggg@gmail.com>
2023-12-15 22:20:36 +0200

    add header

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,28 +1,43 @@
-
 export function Home() {
+    return (
+        <section>
+            <div className="hero">
+                <div className="logo-box">
+                    <img
+                        src="../images/logo-white.png"
+                        alt="logo"
+                        className="logo"
+                    />
+                </div>
 
-  return (
-    <section className="home">
+                <div className="text-box">
+                    <h1 className="heading-primary">
+                        <span className="heading-primary-main">
+                            e-mail and notes
+                        </span>
+                        <span className="heading-primary-sub">
+                            Note your Mail!
+                        </span>
+                    </h1>
 
-      <div className="hero">
+                    <a
+                        href="/#/note/"
+                        className="btn-hero btn-white btn-animated"
+                    >
+                        <img src="images/note.png" className="btn-homepage" />
+                    </a>
+                    <a
+                        href="/#/mail/"
+                        className="btn-hero btn-white btn-animated"
+                    >
+                        <img src="images/email.png" className="btn-homepage" />
+                    </a>
+                </div>
+            </div>
 
-        <div className="logo-box">
-          <img src="../images/logo-white.png" alt="logo" className="logo" />
-        </div>
-        
-        <div className="text-box">
-          <h1 className="heading-primary">
-            <span className="heading-primary-main">e-mail and notes</span>
-            <span className="heading-primary-sub">Note your Mail!</span>
-          </h1>
 
-          <a href="/#/note/" className="btn-hero btn-white btn-animated"><img src="images/note.png"
-            className="btn-homepage" /></a>
-          <a href="/#/mail/" className="btn-hero btn-white btn-animated"><img src="images/email.png"
-            className="btn-homepage" /></a>
-        </div>
 
-      </div>
-    </section>
-  )
+            
+        </section>
+    )
 }
