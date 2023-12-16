b88a7faa2596e5ef2fadffc82d99913aad696b0d
DavidTalRivlin <David.dave.tal@gmail.com>
2023-12-16 23:03:55 +0200

    bug fix

--- views/About.jsx
+++ views/About.jsx
@@ -3,7 +3,7 @@ export function About() {
     return <section className="about">
         <h2 className="about-sentance">We are ABOUT to do this page.........</h2>
         <div className="about-imgs">
-            <img src="./assets/img/david.png" alt="" />
+            <img src="./assets/img/David.png" alt="" />
             <img src="./assets/img/amir.png" alt="" />
         </div>
     </section>
